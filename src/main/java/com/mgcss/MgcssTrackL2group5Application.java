1e8f836f7325d0db29e66e3cbbc1b47f43112c44
ifram <iframeestudios@gmail.com>
2026-03-19 10:26:13 +0100

    feat: add initial project structure

---
+++ src/main/java/com/mgcss/MgcssTrackL2group5Application.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgcssTrackL2group5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgcssTrackL2group5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgcss/MgcssTrackL2group5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcssTrackL2group5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
