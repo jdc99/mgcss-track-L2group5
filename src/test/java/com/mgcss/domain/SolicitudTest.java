f5402718a82238e6569ec677a9a761e31060d771
ifram <iframeestudios@gmail.com>
2026-03-27 14:28:23 +0100

    feat: added tests

---
+++ src/test/java/com/mgcss/domain/SolicitudTest.java
@@ -0,0 +1,64 @@
+package com.mgcss.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolicitudTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getCliente() {
+    }
+
+    @Test
+    void setCliente() {
+    }
+
+    @Test
+    void getDescripcion() {
+    }
+
+    @Test
+    void setDescripcion() {
+    }
+
+    @Test
+    void getEstado() {
+    }
+
+    @Test
+    void setEstado() {
+    }
+
+    @Test
+    void getTecnicoAsignado() {
+    }
+
+    @Test
+    void setTecnicoAsignado() {
+    }
+
+    @Test
+    void getFechaCreacion() {
+    }
+
+    @Test
+    void setFechaCreacion() {
+    }
+
+    @Test
+    void getFechaCierre() {
+    }
+
+    @Test
+    void setFechaCierre() {
+    }
+
+    @Test
+    void differentIds() {
+    }
+}
