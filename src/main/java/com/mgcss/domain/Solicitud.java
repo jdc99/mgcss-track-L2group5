6598efe3271d0241f1a1870141e84ce1a048ab0f
ifram <iframeestudios@gmail.com>
2026-03-27 13:46:41 +0100

    test: added unique id validation in Solicitud

--- src/main/java/com/mgcss/domain/Solicitud.java
+++ src/main/java/com/mgcss/domain/Solicitud.java
@@ -1,8 +1,18 @@
 package com.mgcss.domain;
 
+import java.util.Random;
+
 public class Solicitud {
     //private Estado estado;
     //private Fecha fechaCreacion;
-    private Long id;
+    private final Long id;
+
+    public Solicitud() {
+        this.id = new Random().nextLong();
+    }
+
+    public static boolean differentIds(Solicitud solicitud1, Solicitud solicitud2) {
+        return solicitud1.id.equals(solicitud2.id);
+    }
     // este comentario no sirve para nada
 }
--- src/test/java/com/mgcss/MgcssTrackL2group5ApplicationTests.java
+++ src/test/java/com/mgcss/MgcssTrackL2group5ApplicationTests.java
@@ -1,7 +1,12 @@
 package com.mgcss;
 
+import com.mgcss.domain.Solicitud;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MgcssTrackL2group5ApplicationTests {
@@ -10,4 +15,23 @@ class MgcssTrackL2group5ApplicationTests {
 	void contextLoads() {
 	}
 
+
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    @Test
+    void uniqueIdSolicitud() {
+        Solicitud solicitud1 = new Solicitud();
+        Solicitud solicitud2 = new Solicitud();
+
+        boolean resultado = Solicitud.differentIds(solicitud1,solicitud2);
+        assertFalse(resultado);
+    }
 }
