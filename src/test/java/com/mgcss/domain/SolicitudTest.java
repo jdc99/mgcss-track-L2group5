c98b65598151b0d504871b357e028c0d8461e110
ifram <iframeestudios@gmail.com>
2026-04-13 16:48:18 +0200

    test: TDD - RED failing test for non existing method in Solicitud.java

---
+++ src/test/java/com/mgcss/domain/SolicitudTest.java
@@ -0,0 +1,22 @@
+package com.mgcss.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Date;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolicitudTest {
+
+    @Test
+    void cierreSolicitudRequiereEstadoEnProceso() {
+        Solicitud solicitud = new Solicitud(1L,
+                new Cliente(),
+                "",
+                Date.from(Instant.now()),
+                EstadoSolicitud.ABIERTA,
+                null);
+        assertThrows(IllegalStateException.class, solicitud::cerrarSolicitudEnProceso);
+    }
+}
